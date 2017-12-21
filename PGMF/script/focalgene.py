b5ffdb36f89ccd1a77252db7dd9749eb2a39f507
haiwangyang <haiwangyang@gmail.com>
2017-12-21 17:43:47 -0500

    third

--- PGMF/script/focalgene.py
+++ PGMF/script/focalgene.py
@@ -1,44 +1,149 @@
 #!/usr/bin/env python
 
 """
+Purpose:
 Obtain features of focal gene
 
-Argv:  geneid
+Parameters:
+--species (-s): species
+--geneid (-g): gene id
+--startpeptide (-M): start peptide sequence
+--startpeptidelike (-Ml): start peptide sequence like (allow approximation)
+
+Example command:
+python3 -i focalgene.py -s dper -g MFBST.6354 -Ml MDADSS
+
+Example command (interactive):
+tra = focalgene("dper", "MFBST.6354", "MDADSS")
+tra.isoform['FBtr0183594']["exon"][1]
 """
 
 import re
 import sys
 import os
 import argparse
+from pyfaidx import Fasta
+from Bio.Seq import Seq
 
-parser = argparse.ArgumentParser(description='please provide species and geneid')
-parser.add_argument('-s', '--species', type=str)
-parser.add_argument('-g', '--geneid', type=str)
-args = parser.parse_args()
+def get_revcom_dna(dna):
+    return(str(Seq(dna).reverse_complement()))
+
+def get_pep_and_leftover_from_dna(dna, phase_left):
+    """ translate dna into peptide from phase
+        phase = 0, from the 1st dna base
+        phase = 1, from the 2nd dna base
+        phase = 2, from the 3rd dna base
+        phased peptide and leftover dna on the left & right are returned
+    """
+    dna_leftover_left = dna[:phase_left]
+    phase_right = len(dna[phase_left:]) % 3
+    if phase_right == 0:
+        dna_leftover_right = ""
+        dna_phased = dna[phase_left:]
+    else:
+        dna_leftover_right = dna[-phase_right:]
+        dna_phased = dna[phase_left:-phase_right]
+    pep_phased = str(Seq(dna_phased).translate())
+    return(pep_phased, dna_leftover_left, dna_leftover_right)
 
 class focalgene:
     """focalgene object"""
-    def __init__(self, species, geneid):
+    def __init__(self, species, geneid, startpeptidelike):
         self.species = species
         self.geneid = geneid
-
+        self.startpeptidelike = startpeptidelike
+        self.annotation = self.get_annotation_gene()
+        self.scaffold = self.get_scaffold()
+        self.scaffoldseq = self.get_scaffoldseq()
+        self.isoform = self.get_isoform()
+        self.isoform = self.update_isoform()
+        
     def get_annotation_all(self):
         """ Open annotaiton file """
         with open("../data/annotation/" + self.species + ".tra.gtf", 'r') as f:
-            file_contents = f.read()
-            return file_contents
+            lines = f.readlines()
+            return lines
 
     def get_annotation_gene(self):
-        """ Get annotation of gene """
-        for line in self.get_annotation_all().split():
-            print(line)
+        """ Get annotation of focalgene """
+        lines_of_focalgene = []
+        for line in self.get_annotation_all():
+            (scaffold, tag, feature, start, end, scoare, strand, dot, others) = line.rstrip().split("\t")
+            this_geneid = re.search('gene_id "(.+?)"', others).group(1)
+            if self.geneid == this_geneid:
+                lines_of_focalgene.append(line)
+        return lines_of_focalgene	
 
+    def get_scaffold(self):
+        """ Get scaffold of focalgene """
+        return self.annotation[0].split("\t")[0]
+
+    def get_scaffoldseq(self):
+        """ Get chromosome sequence of the focal gene """
+        return Fasta("../data/genome/" + self.species + ".tra.fasta", as_raw = True)[self.scaffold]    
+  
+    def get_isoform(self):
+        """ Get raw isoform information """
+        transid2info = dict()
+        for line in self.annotation:
+            (scaffold, tag, feature, start, end, scoare, strand, dot, others) = line.rstrip().split("\t")
+            this_transid = re.search('transcript_id "(.+?)"', others).group(1)
+            if feature == "exon":
+                if not this_transid in transid2info.keys():
+                    transid2info[this_transid] = dict()
+                    transid2info[this_transid]["strand"] = strand
+                    transid2info[this_transid]["exonrange"] = set()
+                start = int(start)
+                end = int(end)
+                transid2info[this_transid]["exonrange"].add((start, end))
+        return transid2info
+
+    def update_isoform(self):
+        """ Update isoform information:
+            (1) reorder exon ATG to TAA
+            (2) index exon
+            (3) fetch sequence
+        """
+        transid2updatedinfo = dict()
+        for transid in self.isoform.keys():
+            transid2updatedinfo[transid] = dict()
+            transid2updatedinfo[transid]["exon"] = dict()   
+            strand = self.isoform[transid]["strand"]
+            transid2updatedinfo[transid]["strand"] = strand
+            sorted_exonrange = list(self.isoform[transid]["exonrange"])
+            if strand == "+":
+                sorted_exonrange.sort(key = lambda x: x[0])
+            elif strand == "-":
+                sorted_exonrange.sort(key = lambda x: x[1], reverse = True)
+
+            exonindex = 1
+            for s, e in sorted_exonrange:
+                transid2updatedinfo[transid]["exon"][exonindex] = dict()
+                transid2updatedinfo[transid]["exon"][exonindex]["range"] = [s, e]      
+                if strand == "+":
+                    exonseq = self.scaffoldseq[s-1:e]
+                elif strand == "-":
+                    exonseq = get_revcom_dna(self.scaffoldseq[s-1:e])
+                transid2updatedinfo[transid]["exon"][exonindex]["dnaseq"] = exonseq
+                transid2updatedinfo[transid]["exon"][exonindex]["pepseq"] = dict()
+                for phase_left in range(0,3):
+                    transid2updatedinfo[transid]["exon"][exonindex]["pepseq"][phase_left] = get_pep_and_leftover_from_dna(exonseq, phase_left)
+                exonindex += 1
+        return transid2updatedinfo
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='please provide species and geneid')
     parser.add_argument('-s', '--species', type=str)
     parser.add_argument('-g', '--geneid', type=str)
+    parser.add_argument('-M', '--startpeptide', type = str)
+    parser.add_argument('-Ml', '--startpeptidelike', type = str)
     args = parser.parse_args()
-    tra = focalgene(args.species, args.geneid)
-    tra.get_annotation_gene()
+    tra = focalgene(args.species, args.geneid, args.startpeptidelike)
+    #tra.get_annotation_gene()
+    #print(tra.annotation)
+    #print(tra.scaffold)
+    #print(tra.scaffoldseq)
+    #print(tra.isoform)
 
+
+         
