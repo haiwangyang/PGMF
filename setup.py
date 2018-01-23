2a0c1db43e59ca652944999dd21e8bed65b0797b
haiwangyang <haiwangyang@gmail.com>
2018-01-23 10:42:36 -0500

    m

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='PGMF',
+      version='1.0.0',
+      description='predict gene model frames for multiple tissues, sexes, and species',
+      url='http://github.com/haiwangyang/PGMF',
+      author='Haiwang Yang',
+      author_email='haiwangyang@gmail.com',
+      license='MIT',
+      packages=['PGMF'],
+      install_requires=['urllib.request', 'argparse', 'pathlib', 'pyfaidx', 'Bio.Seq', 'pandas'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
