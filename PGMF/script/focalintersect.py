cfa8024e7e52a623701e2668794af4ba704451e2
haiwangyang <haiwangyang@gmail.com>
2018-01-25 20:02:26 -0500

    m

--- PGMF/script/focalintersect.py
+++ PGMF/script/focalintersect.py
@@ -59,6 +59,103 @@ def get_exons(chromStart, chromEnd, blockSizes, blockStarts):
         exons.append([exonStart, exonEnd])
     return(exons)
 
+""" convert [[1, 2], [3, 4]] to {'3.4', '1.2'} """
+convert_2dlst_to_set = lambda lst: set([float(str(i) + "." + str(ii)) for i,ii in lst])
+convert_set_to_2dlst = lambda sat: [list(map(int, i.split("."))) for i in sat]
+
+def return_first_and_last_exon(exons):
+    """ float format of exons
+        {7.8, 5.6, 3.4, 1.2}
+        only consider genome coordinate
+        return (1.2, 7.8)
+    """
+    lst_exons = list(exons)
+    lst_exons.sort(key=int)
+    return(lst_exons[0], lst_exons[-1])    
+
+
+def report_exon_overlap(strand1, exons1, strand2, exons2):
+    """ report summary of exons1 exons2 overlap """
+    #print(strand1)
+    #print(exons1)
+    #print(exons2)
+    exons1 = convert_2dlst_to_set(exons1)
+    first_exon1, last_exon1 = return_first_and_last_exon(exons1)
+    exons2 = convert_2dlst_to_set(exons2)
+    first_exon2, last_exon2 = return_first_and_last_exon(exons2)
+    
+    dct_report = dict()
+    if not first_exon1 == first_exon2:
+        """ first exon of isoseq and annotated-gene-model are not exactly the same """
+        if str(first_exon1).split(".")[1] == str(first_exon2).split(".")[1]:
+            """ if first intron-start boundary is the same """
+            if int(str(first_exon1).split(".")[0]) > int(str(first_exon2).split(".")[0]):
+                """ if isoseq first exon is shorter """
+                if strand1 == "+":
+                    dct_report[5] = "partial_inside"
+                else:
+                    dct_report[3] = "partial_inside"
+            else:
+                """ if isoseq first exon is longer """
+                if strand1 == "+":
+                    dct_report[5] = "partial_outside"
+                else:
+                    dct_report[3] = "partial_outside"
+        else:
+            if strand1 == "+":
+                dct_report[5] = "different"
+            else:
+                dct_report[3] = "different"
+    else:
+        if strand1 == "+":
+            dct_report[5] = "same"
+        else:
+            dct_report[3] = "same"
+
+    if not last_exon1 == last_exon2:
+        """ last exon of isoseq and annotated-gene-model are not exactly the same """
+        if str(last_exon1).split(".")[0] == str(last_exon2).split(".")[0]:
+            """ if last intron-end boundary is the same """
+            if int(str(last_exon1).split(".")[1]) < int(str(last_exon2).split(".")[1]):
+                """ if isoseq first exon is shorter """
+                if strand1 == "+":
+                    dct_report[3] = "partial_inside"
+                else:
+                    dct_report[5] = "partial_inside"
+            else:
+                """ if isoseq first exon is longer """
+                if strand1 == "+":
+                    dct_report[3] = "partial_outside"
+                else:
+                    dct_report[5] = "partial_outside"
+        else:
+            if strand1 == "+":
+                dct_report[3] = "different"
+            else:
+                dct_report[5] = "different" 
+    else:
+        if strand1 == "+":
+            dct_report[3] = "same"
+        else:
+            dct_report[5] = "same"
+    return(dct_report[5], dct_report[3])
+
+def summarize_jaccard(sample):
+    """ generate summary of jaccard for each isoseq """
+    (species, sex, tissue, replicate) = sample.split("_")
+    ins = FocalIntersect(species, sex, tissue, replicate)
+    with open("../data/output/" + ins.name + ".A.txt", 'w') as f:
+        for isoseqid in ins.jaccard_infoA.keys():
+             intersection_sum, union_sum, jaccard = ins.jaccard_infoA[isoseqid]
+             transid = ins.isoseqid2besttransidA[isoseqid]
+             f.write(isoseqid + "\t" + transid + "\t" + str(intersection_sum) + "\t" + str(union_sum) + "\t" + str(jaccard) + "\n")
+
+    with open("../data/output/" + ins.name + ".B.txt", 'w') as f:
+        for isoseqid in ins.jaccard_infoB.keys():
+             intersection_sum, union_sum, jaccard = ins.jaccard_infoB[isoseqid]
+             transid = ins.isoseqid2besttransidB[isoseqid]
+             f.write(isoseqid + "\t" + transid + "\t" + str(intersection_sum) + "\t" + str(union_sum) + "\t" + str(jaccard) + "\n")
+
 class FocalIntersect:
     """FocalIntersect object"""
     def __init__(self, species, sex, tissue, replicate):
@@ -101,9 +198,16 @@ class FocalIntersect:
         
         """ get jaccard info such as intersection union jaccard for each isoseqid """
         print("get jaccard A info ...")
-        self.jaccard_infoA = self.get_jaccard_info("A")
+        # self.jaccard_infoA = self.get_jaccard_info("A")
         print("get jaccard B info ...")
-        self.jaccard_infoB = self.get_jaccard_info("B")
+        # self.jaccard_infoB = self.get_jaccard_info("B")
+        
+        """ get end info for each isoseqid """
+        print("get end A info ...")
+        self.end_infoA = self.get_end_info("A")
+        print("get end B info ...")
+        self.end_infoB = self.get_end_info("B")
+
 
     def get_intersectinfo(self, gtf_version):
         if gtf_version == "A":
@@ -144,11 +248,44 @@ class FocalIntersect:
             transids = isoseqid2transid[isoseqid]
             this_transid2exonlen = dict()
             for transid in transids:
-                exonlen = int(transid2exonlen[transid])             
+                exonlen = int(transid2exonlen[transid])
+                #print(gtf_version, transid, str(exonlen))    
                 this_transid2exonlen[transid] = exonlen
             inverse = [(value, key) for key, value in this_transid2exonlen.items()]
             isoseqid2besttransid[isoseqid] = max(inverse)[1]         
         return(isoseqid2besttransid)
+
+    def get_end_info(self, gtf_version):
+        """ get the end info for isoseq cmp annotation gene model """
+        if gtf_version == "A":
+            isoseqid2besttransid = self.isoseqid2besttransidA
+            isoseqidtransid2info = self.isoseqidtransidA2info
+        elif gtf_version == "B":
+            isoseqid2besttransid = self.isoseqid2besttransidB
+            isoseqidtransid2info = self.isoseqidtransidB2info
+
+        isoseqid2endinfo = dict()
+        count = 0
+        for isoseqid in isoseqid2besttransid.keys():
+            if count % 1000 == 0:
+                print(count)
+            count += 1
+            transid = isoseqid2besttransid[isoseqid]
+            line = isoseqidtransid2info[isoseqid + "." + transid]
+            #print("#################################")
+            #print(isoseqid, transid, line)
+            (chrom1, chromStart1, chromEnd1, isoseqid, score1, strand1, thickStart1, thickEnd1, itemRgb1, blockCount1, blockSizes1, blockStarts1, chrom2, chromStart2, chromEnd2, transid, score2, strand2, thickStart2, thickEnd2, itemRgb2, blockCount2, blockSizes2, blockStarts2, intersection)  = line.rstrip().split("\t")
+            chromStart1 = int(chromStart1)
+            chromEnd1 = int(chromEnd1)
+            chromStart2 = int(chromStart2)
+            chromEnd2 = int(chromEnd2)
+            exons1 = get_exons(chromStart1, chromEnd1, blockSizes1, blockStarts1)
+            exons2 = get_exons(chromStart2, chromEnd2, blockSizes2, blockStarts2)
+            minStart =  min(chromStart1, chromStart2)
+            maxEnd = max(chromEnd1, chromEnd2)
+            five_prime_end, three_prime_end = (report_exon_overlap(strand1, exons1, strand2, exons2))
+            isoseqid2endinfo[isoseqid] = [str(len(exons1)), str(len(exons2)), five_prime_end, three_prime_end]
+        return(isoseqid2endinfo)
 
     def get_jaccard_info(self, gtf_version):
         """ get the transid with the largest overlap """
@@ -167,7 +304,8 @@ class FocalIntersect:
             count += 1
             transid = isoseqid2besttransid[isoseqid]
             line = isoseqidtransid2info[isoseqid + "." + transid]
-            # print(isoseqid, transid, line)
+            print("#################################")
+            print(isoseqid, transid, line)
             (chrom1, chromStart1, chromEnd1, isoseqid, score1, strand1, thickStart1, thickEnd1, itemRgb1, blockCount1, blockSizes1, blockStarts1, chrom2, chromStart2, chromEnd2, transid, score2, strand2, thickStart2, thickEnd2, itemRgb2, blockCount2, blockSizes2, blockStarts2, intersection)  = line.rstrip().split("\t")
             chromStart1 = int(chromStart1)
             chromEnd1 = int(chromEnd1)
@@ -177,7 +315,7 @@ class FocalIntersect:
             exons2 = get_exons(chromStart2, chromEnd2, blockSizes2, blockStarts2)
             minStart =  min(chromStart1, chromStart2)
             maxEnd = max(chromEnd1, chromEnd2)
-            #print(chromStart1, chromEnd1, blockSizes1, blockStarts1, exons1)
+
             union_sum = 0
             intersection_sum = 0
             for i in range1(minStart, maxEnd):
@@ -192,17 +330,19 @@ class FocalIntersect:
         return(isoseqid2jaccardinfo)
 
 if __name__ == '__main__':
-    name = sys.argv[1] # dmel_f_wb_r1
-    (species, sex, tissue, replicate) = name.split("_")
-    ins = FocalIntersect(species, sex, tissue, replicate)
-    with open("../data/output/" + ins.name + ".A.txt", 'w') as f:
-        for isoseqid in ins.jaccard_infoA.keys():
-             intersection_sum, union_sum, jaccard = ins.jaccard_infoA[isoseqid]
-             transid = ins.isoseqid2besttransidA[isoseqid]
-             f.write(isoseqid + "\t" + transid + "\t" + str(intersection_sum) + "\t" + str(union_sum) + "\t" + str(jaccard) + "\n")
+    sample = sys.argv[1] # dmel_f_wb_r1
+    # summarize_jaccard(sample)
 
-    with open("../data/output/" + ins.name + ".B.txt", 'w') as f:
-        for isoseqid in ins.jaccard_infoB.keys():
-             intersection_sum, union_sum, jaccard = ins.jaccard_infoB[isoseqid]
+    (species, sex, tissue, replicate) = sample.split("_")
+    ins = FocalIntersect(species, sex, tissue, replicate)
+    with open("../data/output/" + ins.name + ".end.A.txt", 'w') as f:
+        for isoseqid in ins.end_infoA.keys():
+             exonnum1, exonnum2, five_desc, three_desc = ins.end_infoA[isoseqid]
+             transid = ins.isoseqid2besttransidA[isoseqid]
+             f.write(isoseqid + "\t" + transid + "\t" + exonnum1 + "\t" + exonnum2 + "\t" + five_desc + "\t" + three_desc + "\n")
+
+    with open("../data/output/" + ins.name + ".end.B.txt", 'w') as f:
+        for isoseqid in ins.end_infoB.keys():
+             exonnum1, exonnum2, five_desc, three_desc = ins.end_infoB[isoseqid]
              transid = ins.isoseqid2besttransidB[isoseqid]
-             f.write(isoseqid + "\t" + transid + "\t" + str(intersection_sum) + "\t" + str(union_sum) + "\t" + str(jaccard) + "\n")
+             f.write(isoseqid + "\t" + transid + "\t" + exonnum1 + "\t" + exonnum2 + "\t" + five_desc + "\t" + three_desc + "\n")
